378804b90bfa2734316afdf54481de1c24ebe6f8
Xnuiem <Xnuiem@786ddd88-0253-0410-b825-45e23bae4f57>
2011-06-29 21:06:49 +0000

    Initial Commit

---
+++ phpx/phpx.php
@@ -0,0 +1,78 @@
+<?php
+/*
+Plugin Name: phpX
+Plugin URI: http://www.phpx.org
+Description: 
+Version: 0.1
+Author: Xnuiem
+Author URI: http://www.thisrand.com
+
+*/
+
+/*  Copyright 1997-2011 Xnuiem  (email : scripts @T thisrand D07 com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * 
+ * @since 3.0.0
+ */
+
+if (!defined('WP_CONTENT_URL')){ define('WP_CONTENT_URL', get_option('siteurl') . '/wp-content'); }
+if (!defined('WP_CONTENT_DIR')){ define('WP_CONTENT_DIR', ABSPATH . 'wp-content' ); }
+if (!defined('WP_PLUGIN_URL')){  define('WP_PLUGIN_URL', WP_CONTENT_URL. '/plugins' ); }
+if (!defined('WP_PLUGIN_DIR')){  define('WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' ); }
+      
+define(PHPX_DIR, WP_PLUGIN_DIR . '/phpx/');  
+define(PHPX_URL, WP_PLUGIN_URL . '/phpx/'); 
+
+
+
+
+
+
+
+//wp_deregister_script('jquery');
+//wp_deregister_script('jquery-ui-core');
+//wp_register_script('jquery', MULTIX_URL . 'suitex/js/jquery-1.5.1.min.js');
+//wp_register_script('jquery-ui-core', MULTIX_URL . 'suitex/js/jquery-ui-1.8.13.custom.min.js');
+//wp_register_script('jquery-validate', MULTIX_URL . 'suitex/js/jquery.validate.min.js');
+wp_enqueue_script('phpx', PHPX_URL . 'js/phpx.js');
+//wp_localize_script('suitex', 'SuiteXAjax', array('ajaxurl' => admin_url('admin-ajax.php')));
+
+
+
+
+//register_activation_hook(__FILE__, array($obj, 'multix_install'));
+//register_deactivation_hook(__FILE__, array($obj, 'multix_uninstall'));
+add_action('admin_head', 'phpx_addCSS'); 
+add_action('wp_head', 'phpx_addCSS'); 
+function phpx_addCSS(){        
+    if (is_admin()){
+        print('<link type="text/css" rel="stylesheet" href="' . PHPX_URL . 'css/phpx-admin.css" />');         
+    }
+    else {
+        print('<link type="text/css" rel="stylesheet" href="' . PHPX_URL . 'css/phpx.css" />');  
+        print('<meta name="generator" content="phpX Framework" />');
+       
+    }
+}
+
+
+
+
+
+?>
---
+++ phpx/readme.txt
@@ -0,0 +1,39 @@
+=== phpX ===
+Contributors: Xnuiem
+Donate link: http://www.phpx.org
+Tags: automatic, integration, plugin, admin
+Requires at least: 3.0.0
+Tested up to: 3.1.3
+Stable tag: 0.1
+
+
+
+== Description ==
+
+
+
+== Installation ==
+
+1. Upload the multix directory to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. In order to create a trust relationship between sites, MultiX must be installed on each instance of Wordpress.
+4. Once MultiX is installed on each instance of Wordpress you wish to create a relationship with, click the "Add Site" link at the top of the MultiX admin screen (Tools->MultiX)
+5. If you have cURL installed, you will see a form, "Automatically Add Site".  Use that form to add another site.  
+6. If you do not have cURL installed, you will have to create the trust relationship manually be exchanging site keys between each site.  
+
+== Frequently Asked Questions ==
+
+== Change Log ==
+
+<b>0.1</b><br />
+Initial Version
+<br />
+
+== Screenshots==
+
+== Demos ==
+
+== To Do ==
+
+
+
